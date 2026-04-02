9f74c096b121383da35012235f7abf8aefffd143
Lakshay Sharma <lakshaysharma147258@gmail.com>
2026-04-02 21:30:59 +0530

    fix mongo uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      process.env.MONGO_URL
-    )
-    .then(() =>console.log("DB Connected"));
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("DB Connected"));
 };
