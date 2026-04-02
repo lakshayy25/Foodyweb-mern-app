99e3bccfab44084232896ee990d8b4a185aaec16
Lakshay Sharma <lakshaysharma147258@gmail.com>
2026-04-02 22:18:17 +0530

    added missing app files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>🍔 Food Delivery App</h1>
+      <p>Backend Connected Successfully 🚀</p>
+    </div>
+  );
+};
+
+export default App;
